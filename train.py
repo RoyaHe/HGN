43ca286608f16df513f7b47a08edd3d05a62a259
RoyaHe <51139394+RoyaHe@users.noreply.github.com>
2021-07-17 00:18:24 +0800

    Update train.py

--- train.py
+++ train.py
@@ -52,7 +52,7 @@ dev_feature_dict = helper.dev_feature_dict
 dev_dataloader = helper.dev_loader
 
 ##########################################################################
-# Add type I que_sent nodes into train_dataloader, dev_dataloader
+# Add type III que_sent nodes into train_dataloader, dev_dataloader
 ##########################################################################
 ## There 105 nodes in the graph
 ## - 1 question node
@@ -65,22 +65,21 @@ dev_dataloader = helper.dev_loader
 for id in train_dataloader.graph_dict:
 
   adj_id = train_dataloader.graph_dict[id]['adj']
-  indx_entities = np.arange(105)[adj_id[0,:] == 2]
+  indx_entities = np.arange(105)[adj_id[0,:] == 1]
 
   for indx_ent in indx_entities:
-    train_dataloader.graph_dict[id]['adj'][0,:][np.arange(105)[adj_id[:,indx_ent]==7]] = 9
-    train_dataloader.graph_dict[id]['adj'][:,0][np.arange(105)[adj_id[:,indx_ent]==7]] = 9
+    train_dataloader.graph_dict[id]['adj'][0,:][np.arange(105)[adj_id[:,indx_ent]==4]] = 9
+    train_dataloader.graph_dict[id]['adj'][:,0][np.arange(105)[adj_id[:,indx_ent]==4]] = 9
 
- 
+    
 for id in dev_dataloader.graph_dict:
-
+  
   adj_id = dev_dataloader.graph_dict[id]['adj']
-  indx_entities = np.arange(105)[adj_id[0,:] == 2]
+  indx_entities = np.arange(105)[adj_id[0,:] == 1]
 
   for indx_ent in indx_entities:
-    dev_dataloader.graph_dict[id]['adj'][0,:][np.arange(105)[adj_id[:,indx_ent]==7]] = 9
-    dev_dataloader.graph_dict[id]['adj'][:,0][np.arange(105)[adj_id[:,indx_ent]==7]] = 9
-
+    dev_dataloader.graph_dict[id]['adj'][0,:][np.arange(105)[adj_id[:,indx_ent]==4]] = 9
+    dev_dataloader.graph_dict[id]['adj'][:,0][np.arange(105)[adj_id[:,indx_ent]==4]] = 9
 
 #########################################################################
 # Initialize Model
