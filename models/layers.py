c24c9b0f826d9220dd147049b8388cf5b1fd5289
RoyaHe <51139394+RoyaHe@users.noreply.github.com>
2021-10-29 05:51:49 +0800

    Update layers.py

--- models/layers.py
+++ models/layers.py
@@ -188,7 +188,7 @@ class AttentionLayer(nn.Module):
           N,E,d = h.shape
             
           ## query level updates
-          if level == [1,6]:
+          if level == [1,6,8]:
             mask_0 = torch.zeros_like(h)
             mask_0[:,0,:] = torch.ones(N,1,d)
             mask_1 = torch.ones_like(h) - mask_0
@@ -196,7 +196,7 @@ class AttentionLayer(nn.Module):
             h_i = h*mask_0 + h_i*mask_1
             
           ## paragraph level updates
-          if level == [1,2,4,5,8]:
+          elif level == [1,2,4,5,8]:
             mask_0 = torch.zeros_like(h)
             mask_0[:,1:5,:] = torch.ones(N,4,d)
             mask_1 = torch.ones_like(h) - mask_0
